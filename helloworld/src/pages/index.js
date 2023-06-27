ca0de92a6716adb5d4ec02bfb88c7e05e730a51b
eaglementality <jesseowusuyeboah99@gmail.com>
2023-06-27 08:16:25 +0000

    yet to be born

---
+++ helloworld/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import Todolist from '@/components/testUI';
+import TodoApp from '@/components/todoList';
+export default function Homepage(){
+return (<main>
+{/* <Todolist/> */}
+<TodoApp/>
+</main>)
+
+}
