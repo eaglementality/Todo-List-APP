033cbf78e07a0fed042f909b57c5472c84405c0f
eaglementality <jesseowusuyeboah99@gmail.com>
2023-07-04 12:57:56 +0000

    feature(context): add context to share data

--- helloworld/src/pages/_app.js
+++ helloworld/src/pages/_app.js
@@ -1,7 +1,12 @@
 import '@/styles/globals.css'
 // import '../styles/todolist.css';
+import AppContext from '@/context/context'
 
 
 export default function App({ Component, pageProps }) {
-  return  <Component {...pageProps} />
+  return (
+    <AppContext>
+      <Component {...pageProps} />
+    </AppContext>
+  )
 }
