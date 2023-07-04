31012485f2400c11d5bf429d9a09f2ef1d7f7437
eaglementality <jesseowusuyeboah99@gmail.com>
2023-07-04 13:05:34 +0000

    work in progress on main

--- helloworld/src/components/task-card.jsx
+++ helloworld/src/components/task-card.jsx
@@ -1,20 +1,37 @@
-export const TaskCard = ({id, active, done, title}) => {
+import { appProps } from "@/context/context";
 
+export const TaskCard = ({id, active, task, isComplete}) => {
+
+    const {AllTasks, setAllTasks} = appProps();
+   
     const MakeActive = () => {
+        const ifActive = AllTasks.map(task => (task.id === id ? {...task, isActive: !task.isActive}: task))
+        setAllTasks(ifActive);
+    
+    }
+
+    const deleteTask = () => {
+        setAllTasks(AllTasks.filter(item => item.id !== id));
         
     }
 
+    const MarkAsDone = () => {
+        const ifDone = AllTasks.map(task => (task.id === id ? {...task, isComplete: !task.isComplete}: task));
+        setAllTasks(ifDone);
+    }
+
+// console.log(AllTasks);
     return (
 
         <div className="px-5 py-4">
             <div className="flex items-center">
-                <span className='flex items-center border border-zinc-400 rounded-full p-1.5'>
-                    {active &&  <svg  xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="black" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>}                   
-                </span>
-                <span className="ml-3 flex-1">{title}</span>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-                </span>
+                <button type="button" onClick={() => MakeActive()} className='flex items-center border  border-zinc-400 rounded-full p-2.5'>
+                   {active && <svg className='' xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="black" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg> }                  
+                </button>
+                <p onClick={() => MarkAsDone()} className={`${isComplete ? "line-through decoration-slate-300 text-slate-300": ""} ml-3 flex-1`} >{task}</p>
+                <button type="button" onClick={() => deleteTask()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+                </button>
             </div>
         </div>
 
--- helloworld/src/pages/index.js
+++ helloworld/src/pages/index.js
@@ -1,41 +1,57 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from "react";
 import { TaskCard } from "@/components/task-card";
-import Data from "../../data.json";
-
+import { appProps } from "@/context/context";
 
 
 export default function TodoApp(){
 
-    const [AllTasks, setAllTasks] = useState(() => Data.tasks);
+    const {AllTasks, setAllTasks, filter, setFilter} = appProps();
 
-
+    
+    useEffect(() => {
+        
+    }, [filter, AllTasks]);
     const GetAllTasks = () => {
-        const userTasks = AllTasks.map(task => (
+        const userTasks = AllTasks.map((items,i) => (
             <TaskCard
-                id={task.id}
-                active={task.isActive}
-                done={task.lineThrough}
-                title={task.task}
+                key={i}
+                id={items.id}
+                active={items.isActive}
+                task={items.task}
+                isComplete={items.isComplete}
              />
         ))
 
         return userTasks;
     }
-    
+//     const clearcompleted=()=>{
+//         setAllTasks(AllTasks.filter(item => item.isComplete != true));
+//     }
+//     const All_Task = () =>{ 
+//         setAllTasks(AllTasks.map(items => ({...items})))
+//     }
+//     const activeTask = ()=>{
+//         setAllTasks(AllTasks.filter(item => item.isActive === true ));
+//     }
+//     const CompletedTask =()=>{
+//         setAllTasks(AllTasks.filter(item => item.isComplete === true))
+//     }
+// console.log(AllTasks);
+
     return(
     <main className='h-screen w-screen bg-zinc-100'>
         <div className="w-full bg-image px-6">
             <div className="flex items-center justify-between pb-36 pt-12">
                 <span className="text-todo font-bold text-2xl text-white tracking-widest">TODO</span>
                 <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
                 </span>
             </div>
         </div>
         <div className="flex flex-col rounded-lg space-y-6 px-6 -mt-28">
             <div className="flex items-center w-full bg-white rounded-md pl-5 py-3.5 px-5">
-                <span className='flex items-center bg-zinc-200 rounded-full p-1.5'>
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="black" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+                <span className='flex items-center bg-zinc-200 rounded-full px-1.5 py-1.5'>
+                    <svg  xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="black" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
                 </span>
                 <input type="text" name="taskname" className="ml-3 w-full rounded-md outline-none border-none text-zinc-400 placeholder-zinc-400" placeholder="Create a blah blah blah"/>
             </div>
@@ -44,16 +60,16 @@ export default function TodoApp(){
                     {GetAllTasks()}
                     
                 <div className="px-5 pt-4 pb-5 flex justify-between">
-                    <span>5 Items left</span>
-                    <span>Clear completed</span>
+                    <span>{AllTasks.length} Items left</span>
+                    <span >Clear completed</span>
                 </div>
             </div>
 
             <div className="px-20 py-3.5 bg-white rounded-md font-bold tracking-tight text-zinc-400">
                 <div className='flex justify-between'>
-                    <span>All</span>
-                    <span>Active</span>
-                    <span>Completed</span>
+                    <span onClick={() => setFilter("all")}>All</span>
+                    <span onClick={() => setFilter("active")}>Active</span>
+                    <span onClick={() => setFilter("completed")}>Completed</span>
                 </div>
             </div>
         </div>
