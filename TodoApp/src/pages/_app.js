e04402a86568272d1a6339303e367458d91b60b1
Jesse <97172881+eaglementality@users.noreply.github.com>
2023-07-13 15:59:57 +0000

    Updated _app.js

--- TodoApp/src/pages/_app.js
+++ TodoApp/src/pages/_app.js
@@ -1,15 +1,13 @@
 import '@/styles/globals.css'
 import '../styles/todolist.css';
 import AppContext from '@/utils/context'
-import {ThemeProvider} from 'next-themes'
+
 
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute='class'>
     <AppContext>
       <Component {...pageProps} />
-    </AppContext>
-    </ThemeProvider>
+    </AppContext>   
   )
 }
